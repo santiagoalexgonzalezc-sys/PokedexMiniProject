3fe5323044589d9c2423834d5d88112a85a3f481
santiagoalexgonzalezc-sys <santiagoalexgonzalezc@gmail.com>
2026-05-05 08:22:07 -0400

    Starting giving my information from Json and fixing some css mistakes

--- ProPokeDez.js
+++ ProPokeDez.js
@@ -1,1 +1,18 @@
 //<a href=""><img src="https://img.pokemondb.net/sprites/x-y/normal/bulbasaur.png" alt="Bulbasaur"></a>
+
+
+async function loadData() {
+    try {
+        const response = await fetch('./proPokeDez.json');
+        const data = await response.json();
+
+        const nombre_001 = document.getElementById('nombre_001');
+        nombre_001.textContent = data.Bulbasaur[2].name1;
+
+    } catch (error) {
+        console.error('Error loading JSON:', error);
+    }
+    
+}
+
+loadData()
