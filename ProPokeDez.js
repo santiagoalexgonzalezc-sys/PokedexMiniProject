b99479b8d54fd82220a9dcb7b9c3f4a3ccfcffa2
santiagoalexgonzalezc-sys <santiagoalexgonzalezc@gmail.com>
2026-05-06 19:11:39 -0400

    adding new pokemons and optimizing scroll, with title and more css

--- ProPokeDez.js
+++ ProPokeDez.js
@@ -3,7 +3,6 @@ const go_Kanto = document.getElementById("go_Kanto")
 const home_Section = document.getElementById("home_Section")
 const kanto_Region_Section = document.getElementById("kanto_Region_Section")
 
-const inputBulbasaur = document.getElementById("Bulbasaur_input") 
 
 let current_charateristic
 
@@ -24,10 +23,7 @@ go_Kanto.addEventListener('click', goToKantoFunction)
 
 loadData()
 
-if (inputBulbasaur.checked) {
-      current_charateristic  = inputBulbasaur.id
-      print(current_charateristic)
-}
+
 
 function goToKantoFunction() {
     home_Section.style.display = "none"
