8980fe6624a915da159ba3be21c04e1584fc1875
santiagoalexgonzalezc-sys <santiagoalexgonzalezc@gmail.com>
2026-05-06 21:29:07 -0400

    Fixing id classesm alts and numbers

--- ProPokeDez.js
+++ ProPokeDez.js
@@ -6,22 +6,19 @@ const kanto_Region_Section = document.getElementById("kanto_Region_Section")
 
 let current_charateristic
 
-async function loadData() {
-    try {
-        const response = await fetch('./proPokeDez.json');
-        const data = await response.json();
-
-        const nombre_001 = document.getElementById('nombre_001');
-        nombre_001.textContent = data.Bulbasaur[2].name1;
-
-    } catch (error) {
-        console.error('Error loading JSON:', error);
-    }
+//async function loadData() {
+//    try {
+ //       const response = await fetch('./proPokeDez.json');
+  //      const data = await response.json();
+//
+ //   } catch (error) {
+  //      console.error('Error loading JSON:', error);
+   // }
     
-}
+//}
 go_Kanto.addEventListener('click', goToKantoFunction)
 
-loadData()
+//loadData()
 
 
 
